41e7a3bf75d87527586ff7450e6539f53bedabee
kipchirchirkelvin@gmail.com <kipchirchirkelvin@gmail.com>
2024-06-20 15:16:54 +0300

    first commit

---
+++ datatype.py
@@ -0,0 +1,33 @@
+number = 78 # Int
+x = 67.32 # Float
+greeting = "Hello there" # String
+isPythonInteresting = True # Bool
+devices = ["laptop","computer","phone"] # List values are changable
+browsers =("Firefox","Chrome","Safari","opera") #Tuple values are unchangable
+countries ={"Kenya","Sudan","Rwanda"} # Set values are unorderd
+student ={
+
+    "firstname":"kelvin",
+    "age":18,
+    "course":"MIT"
+
+} # This is a dictonary
+
+print(isPythonInteresting )
+print(x)
+print(devices)
+print(student["firstname"])
+print(student["age"])
+print(student["course"])
+print(countries)
+print(countries)
+
+
+
+#Determining the data type of  a variable
+print(type(devices))
+print(type(student))
+
+# Typecasting is the process of converting one data type into another
+print(int(x))
+print(float(number))
---
+++ variables.py
@@ -0,0 +1,27 @@
+# variable is a container used to store a value
+
+name = "Kelvin"
+programming_school = "emobilis"
+
+
+
+print(programming_school)
+
+print(name)
+
+num = 67
+num2 = 3
+age = 67
+
+print(name)
+print(67)
+
+print(name)
+print(num)
+print("my name is ", name)
+
+print ("my name is ", name, "I study at",programming_school)
+print(" I am ",age, "years old.")
+
+print("The sum of ", num,"and",num2,"is",num+num2)
+print("the sum of ",num2,"and",num2, "is" ,num2+num2)
